e532c31b36a6060ed5b7b48b1a8bdea279b0bbcf
taylor <liang.tang@cootek.cn>
2020-05-14 11:39:28 +0800

    Refactor: make demo more straightforward

--- app/src/main/java/taylor/com/layout_dsl/MainActivity.kt
+++ app/src/main/java/taylor/com/layout_dsl/MainActivity.kt
@@ -13,5 +13,10 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        supportFragmentManager.beginTransaction().apply {
+            add(R.id.container, FirstFragment(), "first")
+            commit()
+        }
     }
 }
