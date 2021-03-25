a6395448160fffd164c98cf8a3f4cbd299e1313a
wisdomtl <l.tang@neoclub.cn>
2021-03-25 15:37:04 +0800

    Fix: failed to store viewScope in View's tag

--- app/src/main/java/taylor/com/dsl/Layout.kt
+++ app/src/main/java/taylor/com/dsl/Layout.kt
@@ -2317,7 +2317,7 @@ val View.viewScope: CoroutineScope
         var scope = getTag(key) as? CoroutineScope
         if (scope == null) {
             scope = CoroutineScope(SupervisorJob() + Dispatchers.IO)
-            tag = key
+            setTag(key, scope)
             val listener = object : View.OnAttachStateChangeListener {
                 override fun onViewAttachedToWindow(v: View?) {
                 }
